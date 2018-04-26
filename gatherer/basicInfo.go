909b8a40054404d3765addc90285d45930b94fae
AmyangXYZ <amyang.xyz@gmail.com>
2018-04-26 12:59:26 +0800

    retrieve header

--- gatherer/basicInfo.go
+++ gatherer/basicInfo.go
@@ -1,6 +1,7 @@
 package gatherer
 
 import (
+	"io/ioutil"
 	"net"
 	"net/http"
 	"strings"
@@ -11,9 +12,13 @@ import (
 // BasicInfo gathers basic information of the target.
 // AJAX API.
 type BasicInfo struct {
-	target    string
-	IPAddr    string
-	WebServer string
+	target                  string
+	IPAddr                  string
+	WebServer               string
+	ClickJackingProtection  bool
+	ContentSecurityPolicy   bool
+	XContentTypeOptions     bool
+	StrictTransportSecurity bool
 }
 
 // NewBasicInfo returns a basicInfo gatherer.
@@ -30,8 +35,12 @@ func (bi *BasicInfo) Set(v ...interface{}) {
 // Report implements Gatherer interface
 func (bi *BasicInfo) Report() map[string]interface{} {
 	return map[string]interface{}{
-		"ip":        bi.IPAddr,
-		"webserver": bi.WebServer,
+		"ip":                        bi.IPAddr,
+		"webserver":                 bi.WebServer,
+		"click_jacking_protection":  bi.ClickJackingProtection,
+		"content_security_policy":   bi.ContentSecurityPolicy,
+		"x_content_type_options":    bi.XContentTypeOptions,
+		"strict_transport_security": bi.StrictTransportSecurity,
 	}
 }
 
@@ -40,8 +49,12 @@ func (bi *BasicInfo) Run() {
 	bi.resolveIP()
 	logger.Green.Println("IP Address:", bi.IPAddr)
 
-	bi.getWebServer()
+	bi.retrieveHeader()
 	logger.Green.Println("Web Server:", bi.WebServer)
+	logger.Green.Println("Click Jacking Protection:", bi.ClickJackingProtection)
+	logger.Green.Println("Content Security Policy:", bi.ContentSecurityPolicy)
+	logger.Green.Println("X Content Type Options:", bi.XContentTypeOptions)
+	logger.Green.Println("Strict Transport Security", bi.StrictTransportSecurity)
 }
 
 func (bi *BasicInfo) resolveIP() {
@@ -57,14 +70,25 @@ func (bi *BasicInfo) resolveIP() {
 	bi.IPAddr = remoteAddr.String()
 }
 
-func (bi *BasicInfo) getWebServer() {
-	resp, err := http.Head("http://" + bi.target)
+func (bi *BasicInfo) retrieveHeader() {
+	resp, err := http.Get("http://" + bi.target)
 	if err != nil {
-		resp, err = http.Get("http://" + bi.target)
-		if err != nil {
-			logger.Red.Println(err)
-			return
-		}
+		logger.Red.Println(err)
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if len(resp.Header["X-Frame-Options"]) > 0 {
+		bi.ClickJackingProtection = true
+	}
+	if len(resp.Header["Content-Security-Policy"]) > 0 ||
+		strings.Contains(string(body), `http-equiv="Content-Security-Policy"`) {
+		bi.ContentSecurityPolicy = true
+	}
+	if len(resp.Header["X-Content-Type-Options"]) > 0 {
+		bi.XContentTypeOptions = true
+	}
+	if len(resp.Header["Strict-Transport-Secruity"]) > 0 {
+		bi.StrictTransportSecurity = true
 	}
 	bi.WebServer = resp.Header["Server"][0]
 }
