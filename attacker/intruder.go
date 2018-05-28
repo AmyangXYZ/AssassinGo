92e0c54d52588c5f9ed056d967545b0f67a1a783
AmyangXYZ <amyang.xyz@gmail.com>
2018-05-28 16:26:21 +0800

    fix intruder error

--- attacker/intruder.go
+++ attacker/intruder.go
@@ -89,7 +89,7 @@ func (i *Intruder) fetch(payload string) *http.Response {
 	resp, err := client.Do(req)
 	if err != nil {
 		logger.Red.Println(err)
-		return nil
+		return &http.Response{}
 	}
 	return resp
 }
@@ -108,16 +108,20 @@ func (i *Intruder) parse(payload string) *http.Request {
 	}
 
 	hr := strings.Split(x[0], "\n")
-
+	if len(hr) < 2 {
+		logger.Red.Println("invalid header")
+	}
 	method := strings.Split(hr[0], " ")[0]
 	path := strings.Split(hr[0], " ")[1]
-	req, _ := http.NewRequest(method, "http://"+i.target+":8888"+path, strings.NewReader(data))
+	req, _ := http.NewRequest(method, "http://"+i.target+path, strings.NewReader(data))
 
 	for _, row := range hr[1:] {
 		hh := strings.Split(row, ": ")
-		k := hh[0]
-		v := hh[1]
-		req.Header.Add(k, v)
+		if len(hh) > 1 {
+			k := hh[0]
+			v := hh[1]
+			req.Header.Add(k, v)
+		}
 	}
 	return req
 }
